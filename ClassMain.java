5d750c54947115a1386960b5c009b531c0517b67
Bureacov Ecaterina <kitty_2000@rambler.ru>
2020-10-29 14:58:35 +0200

    Methods 1 and 2 added

--- ClassMain.java
+++ ClassMain.java
@@ -3,11 +3,11 @@ public class Main {
 public int a = 15;
 public boolean control;
 
-public void Metod1.2() {
+public void Metod1.3() {
 	
 };
 
-privat int Metod2.2(arg){
+privat int Metod2.3(arg){
 	
 };
 
